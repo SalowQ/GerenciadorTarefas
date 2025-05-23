62178a788653673810f4b795d554e4bb7ff6d69a
Igor Sousa <arqurb.igor@gmail.com>
2025-05-23 10:48:27 -0300

    feat: criar enums de status e prioridade

---
+++ GerenciadorTarefas.Communication/Enums/Status.cs
@@ -0,0 +1,11 @@
+﻿namespace GerenciadorTarefas.Communication.Enums
+{
+    public enum Status
+    {
+        NotStarted = 0,
+        InProgress = 1,
+        Completed = 2,
+        OnHold = 3,
+        Cancelled = 4,
+    }
+}
